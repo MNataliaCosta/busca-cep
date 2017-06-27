5ba82ee887ab1108e634fec99a7f6da1cf324b24
Paulo Costa <paulocosta@MacBook-Pro-de-Paulo.local>
2017-06-27 15:17:12 -0300

    Estrutura busca cep

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +21,15 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+});
+
+app.controller('saberController', function($scope){
+  $scope.saberLigado = false;
+  var som = new Audio('light-saber.mp3');
+
+  $scope.alterarSaber = function (){
+    som.play();
+    $scope.saberLigado = !$scope.saberLigado;
+  }
+
 })
